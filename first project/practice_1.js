451febd212dde75fd1a18c8d069118d21688249f
Alex <alexanderkas88@gmail.com>
2022-05-04 10:57:19 -0300

    New changes as we started practic

---
+++ first project/practice_1.js
@@ -0,0 +1,19 @@
+const numberOfFilmes = +prompt("How many movies have you watched?", '');
+
+const personalMovieDB = {
+    count: numberOfFilmes,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Which movie hav you watched last?'),
+      b = prompt('How can you rate it?'),
+      c = prompt('Which movie hav you watched last?'),
+      d = prompt('How can you rate it?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
