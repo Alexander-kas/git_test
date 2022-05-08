8fd73acf032480167ed8a5b6f2bd076ec2ca9677
suren <suren@suren.com>
2022-05-08 17:52:10 -0600

    Added switch

--- practic 2/practice_2.js
+++ practic 2/practice_2.js
@@ -9,9 +9,11 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < 2; i++){
+let j = 0;
+while(j < 2){
     const a = prompt('Which movie have you watched last?'),
     b = prompt('How can you rate it?');
+    j++;
 
 
     /* a != null - проверяет если пользователь не нажал отмену. ( тогда это true)
@@ -21,22 +23,51 @@ for (let i = 0; i < 2; i++){
     if (a != null && b != null && a != '' && b != '' & a.length < 50){
         personalMovieDB.movies[a] = b;
         console.log('done');
-    }else {
-        console.log('error');
-        i--; //Это значит что если условие не соблюдени то оно вернется на шаг назад и задасть вопросы занаго.
-    }
+}
 }
 
-if (personalMovieDB.count < 10){
-    console.log('Not enoght movies has watched');
-}else if ( personalMovieDB.count >= 10 && personalMovieDB.count <30){
-    console.log('You are a classical watcher');
-}else if (personalMovieDB.count >= 30){
-    console.log('You are a movies addict');
-}else {
-    console.log('Error appears');
+// for (let i = 0; i < 2; i++){
+//     const a = prompt('Which movie have you watched last?'),
+//     b = prompt('How can you rate it?');
+
+
+//     /* a != null - проверяет если пользователь не нажал отмену. ( тогда это true)
+//     /* a != '' - проверяет если пользователь не оставил пустую строку. ( тогда это true)
+//     /* a.length < 50 - проверяет если длина название не более 50 символов. ( тогда это true)
+//     */
+//     if (a != null && b != null && a != '' && b != '' & a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     }else {
+//         console.log('error');
+//         i--; //Это значит что если условие не соблюдени то оно вернется на шаг назад и задасть вопросы занаго.
+//     }
+// }
+
+switch (true){
+    case personalMovieDB.count < 10:
+        console.log('Not enoght movies has watched');
+        break;
+    case personalMovieDB.count >= 10 && personalMovieDB.count <30:
+        console.log('You are a classical watcher');
+        break;
+    case personalMovieDB.count >= 30:
+        console.log('You are a movies addict');
+        break;
+    default:
+        console.log('Error appears');
 }
-console.log(personalMovieDB);
+
+// if (personalMovieDB.count < 10){
+//     console.log('Not enoght movies has watched');
+// }else if ( personalMovieDB.count >= 10 && personalMovieDB.count <30){
+//     console.log('You are a classical watcher');
+// }else if (personalMovieDB.count >= 30){
+//     console.log('You are a movies addict');
+// }else {
+//     console.log('Error appears');
+// }
+// console.log(personalMovieDB);
 
 
 
