f8ee8f83ded2d033f3714714daef1a8baa3d2e5a
suren <suren@suren.com>
2022-05-19 15:10:36 -0600

    new lessons

---
+++ homeWork/exercise_11-2.js
@@ -0,0 +1,18 @@
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof(str) !== 'string') {
+        return "Ошибка!";
+    }
+    // Самый оптимальный вариант решения
+    return str.split('').reverse().join('');
+
+    // Решение при помощи цикла
+    // let newStr = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     newStr += str[i];
+    // }
+    // return newStr
+}
+
+reverse(someString);
